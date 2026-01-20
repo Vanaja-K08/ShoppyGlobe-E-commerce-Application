ea132580fa4bc631615842fd181a9f6db87f09d9
Vanaja-K08 <vanajakoppad@gmail.com>
2026-01-20 20:58:08 +0530

    using route to connect files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import NotFound from "./pages/NotFound";
 
 function App() {
-
   return (
-    <>
-     <h1>ShoppyGlobe App Running</h1>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
