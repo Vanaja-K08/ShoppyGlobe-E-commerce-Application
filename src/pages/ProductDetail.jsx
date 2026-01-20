734937c1967aefadab4fbf876667a7bc2dd70aec
Vanaja-K08 <vanajakoppad@gmail.com>
2026-01-20 21:07:12 +0530

    created product detail page

---
+++ src/pages/ProductDetail.jsx
@@ -0,0 +1,25 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+function ProductDetail() {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://dummyjson.com/products/${id}`)
+      .then(res => res.json())
+      .then(data => setProduct(data));
+  }, [id]);
+
+  if (!product) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h2>{product.title}</h2>
+      <p>{product.description}</p>
+      <p>₹ {product.price}</p>
+    </div>
+  );
+}
+
+export default ProductDetail;
